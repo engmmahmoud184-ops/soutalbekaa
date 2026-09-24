e0604637f5f5b99a8dec9348bc9b24f7634ef0fd
Mohamad Mahmoud <eng.m.mahmoud184@gmail.com>
2026-09-25 01:03:30 +0300

    Connect website to Firebase project

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
 // انسخ إعدادات تطبيق الويب من Firebase Console وضعها هنا.
 // سيعمل الموقع ببيانات تجريبية تلقائياً إلى أن يتم إدخال القيم الحقيقية.
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.firebasestorage.app",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyA85EkH87Fu8MwuqlSZoyq-vlLI4jmJg08",
+  authDomain: "soutalbekaa.firebaseapp.com",
+  projectId: "soutalbekaa",
+  storageBucket: "soutalbekaa.firebasestorage.app",
+  messagingSenderId: "1096442938869",
+  appId: "1:1096442938869:web:101158744bd1f5238d4cbc"
 };
 
 export const isFirebaseConfigured = !Object.values(firebaseConfig).some(value => value.includes("YOUR_"));
